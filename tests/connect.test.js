334f4d256131ab482001324d9a01a421247b2feb
scottroot <allanscotthendrix@gmail.com>
2024-06-13 23:22:39 -0400

    add folder for general tests

---
+++ tests/connect.test.js
@@ -0,0 +1,10 @@
+import { connect } from "../dist/index.js";
+// { result, results, message, spawn, monitor, unmonitor, dryrun, assign }
+
+const args = { GATEWAY_URL: "google.com", MU_URL: "google.com", CU_URL: "google.com", GRAPHQL_URL: "google.com" };
+
+try {
+  // const app = connect(args);
+} catch (e) {
+
+}
